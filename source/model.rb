c4725210ce1b480c8c2b021d6a2b7f9a9a5f9722
SputterPuttRedux <dstanfield.esq@gmail.com>
2014-11-28 17:53:46 -0500

    Test methods in pry. All work

--- source/model.rb
+++ source/model.rb
@@ -21,55 +21,79 @@ end
 #========================= MODEL=========================
 
 class FlashCard
-  # Models our flashcards
+  # Models our flashcard objects
   attr_reader :category, :question, :answer
   attr_accessor :answered
 
   def initialize(args = {})
+  # generates flashcard objects based on hash keys (csv fields)
     @category = args[:category]
     @question = args[:question]
     @answer = args[:answer]
-    # generate object based on hash keys (csv fields)
   end
 end
 
-class CheckCard
-  #will take a user's guess and compare it against the actual answer
 
-  def initialize(card, guess)
-    @card = card
-    @guess = guess
+class Dealer
+   include CSVParser
+
+  # Pull data from the csv and parses into hashes
+   def initialize
+    @file = import("game_questions.csv")
   end
 
-  def self.check
-    #is it bad that we're calling the FlashCard class's methods on an instance variable of this class?
-   @guess == @card.answer
-  end
-
-end
-
-class CardLoader
-  include CSVParser
-  attr_accessor
-
-  def initialize
-    @file = import(@file)
-    load_card
-  end
-
-  def self.load_card
+  # Map csv hashes into an individual FlashCard object
+   def load_card #(draw card?)
     @cards_array = @file.map{|row| FlashCard.new(row)}
     @cards_array.shift
   end
+
+  #Check users guess agains the answer for the current card.
+  def check(card, guess)
+    #is it bad that we're calling the FlashCard class's methods on an instance variable of this class?
+   guess == card.answer
+  end
+
 end
 
-  # include CSVParser
-
-  # Pull data from the csv and parse into flashcard objects
-
-  # TODO: pulling only parts of the CSV at the time (as opposed to slurping the entire CSV at once)
-
-  # TODO: make sure repeat flashcards are not shown (ie, store flashcard_id)
-
-
 binding pry
+
+#====================UNREFACTORED/PSEUDOCODE==================
+
+# class CheckCard
+#   #will take a user's guess and compare it against the actual answer
+#   def initialize
+#     @card = card
+#     @guess = guess
+#   end
+
+#   def self.check(card, guess)
+#     #is it bad that we're calling the FlashCard class's methods on an instance variable of this class?
+#    @guess == @card.answer
+#   end
+
+# end
+
+# class CardLoader
+#   include CSVParser
+
+#   def initialize
+#     @file = import("game_questions.csv")
+#     #self.load_card
+#   end
+
+#   def load_card
+#     @cards_array = @file.map{|row| FlashCard.new(row)}
+#     @cards_array.shift
+#   end
+# end
+
+# include CSVParser
+
+
+# TODO: pulling only parts of the CSV at the time (as opposed to slurping the entire CSV at once)
+
+# TODO: make sure repeat flashcards are not shown (ie, store flashcard_id)
+
+
+
