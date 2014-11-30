aa93566e10be98a8f0bf09b124a2986fbd9525a6
Dylan Shine <dylanshinenyc@gmail.com>
2014-11-29 23:30:57 -0500

    Score type

--- source/push.rb
+++ source/push.rb
@@ -2,7 +2,7 @@ percent = 0
 time = rand(0.01..0.04)
 (1..100).each{|x| sleep(time); print "Submitting your score...#{percent+=1}%\r"}
 puts
-print "Score Successfully Submitted."
+print "Score successfully submitted."
 puts
 system 'git add -A > /dev/null 2>&1'
 system 'git commit -m "Updated High Scores" > /dev/null 2>&1'
