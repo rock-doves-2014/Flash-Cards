86f24e4f9d39711fe4060c7a0baec9083a96d2eb
Dylan Shine <dylanshinenyc@gmail.com>
2014-11-29 21:36:22 -0500

    Updated High Scores

--- source/push.rb
+++ source/push.rb
@@ -1,5 +1,6 @@
 print "Submitting your score"
 (1..20).each{|x| sleep(0.1); print "."}
+puts
 system 'git add -A > /dev/null 2>&1'
 system 'git commit -m "Updated High Scores" > /dev/null 2>&1'
 system 'git push origin online_updater > /dev/null 2>&1'
