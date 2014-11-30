2f144cd351b8dd2b3b6757d1385ab2a5d9346e43
Dylan Shine <dylanshinenyc@gmail.com>
2014-11-29 22:06:17 -0500

    Updated High Scores

--- source/push.rb
+++ source/push.rb
@@ -1,6 +1,5 @@
-print "Submitting your score "
 percent = 0
-(1..100).each{|x| sleep(0.05); print "#{percent+=1}%"}
+(1..100).each{|x| sleep 0.03; print "Submitting your score #{percent+=1}% \e[2J"}
 puts
 system 'git add -A > /dev/null 2>&1'
 system 'git commit -m "Updated High Scores" > /dev/null 2>&1'
