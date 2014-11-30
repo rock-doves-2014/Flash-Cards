47e7ea27b37ace7997db46e124397f4bd8111eb9
Dylan Shine <dylanshinenyc@gmail.com>
2014-11-29 23:30:08 -0500

    Faster Loading

--- source/push.rb
+++ source/push.rb
@@ -2,7 +2,8 @@ percent = 0
 time = rand(0.01..0.04)
 (1..100).each{|x| sleep(time); print "Submitting your score...#{percent+=1}%\r"}
 puts
-print "Successfully Submitted."
+print "Score Successfully Submitted."
+puts
 system 'git add -A > /dev/null 2>&1'
 system 'git commit -m "Updated High Scores" > /dev/null 2>&1'
 system 'git push origin online_updater > /dev/null 2>&1'
