51cafa3c26077ac9cb407681aa527a420fd9251a
Dylan Shine <dylanshinenyc@gmail.com>
2014-11-29 21:24:07 -0500

    Updated High Scores

--- source/push.rb
+++ source/push.rb
@@ -1,4 +1,4 @@
 puts "Submitting your score..."
-system 'git add -A'
-system 'git commit -m "Updated High Scores"'
-system 'git push origin online_updater'
+system 'git add -A > /dev/null 2>&1'
+system 'git commit -m "Updated High Scores" > /dev/null 2>&1'
+system 'git push origin online_updater > /dev/null 2>&1'
